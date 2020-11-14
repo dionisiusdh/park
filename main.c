00f008c242b78037da834d1f13dbbe81af63ef55
Dionisius Darryl Hermansyah <dionisius.dh28@gmail.com>
2020-11-14 21:01:37 +0800

    Ketinggalan brou

---
+++ main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "boolean.h"
+#include "main.h"
+#include "mesin/mesinkar.h"
+#include "mesin/mesinkata.h"
+#include "matriks/matriks.h"
+#include "point/point.h"
+
+/******* FUNGSI MAIN *******/
+int main() {
+  /* *********** VARIABEL UTAMA ********** */
+  /* Map */
+  // Map 1 > <  Map 2
+  //  V          V
+  //  ^          ^
+  // Map 4 > < Map 3
+  MATRIKS Map1;
+  MATRIKS Map2;
+  MATRIKS Map3;
+  MATRIKS Map4;
+
+  /* Posisi */
+  POINT PPlayer;
+  POINT POffice;
+  POINT PAntrian;
+  POINT PGerbang_Map1_Kanan;
+  POINT PGerbang_Map1_Bawah;
+  POINT PGerbang_Map2_Kiri;
+  POINT PGerbang_Map2_Bawah;
+  POINT PGerbang_Map3_Atas;
+  POINT PGerbang_Map3_Kiri;
+  POINT PGerbang_Map4_Atas;
+  POINT PGerbang_Map4_Kanan;
+
+  /* Algoritma */
+  showMap(Map1);
+  initGame();
+  initMap(&Map1);
+
+  return 0;
+}
+
+/******* REALISASI FUNGSI-FUNGSI UTAMA *******/
+void initGame() {
+    /**** MAIN MENU ****/
+    printf("// Welcome to Willy wangky's fum factory!!//\n// New game / load game / exit? //\n$ ");
+
+    // Baca masukan 
+    STARTKATA();
+    while (!EndKata) {
+        ADVKATA();  
+    }
+
+    if (CKata.TabKata=='e') {
+        printf("// Thanks for playing!!! //");
+        return 0;
+    }
+}
+
+void initMap(MATRIKS *Map) {
+  BacaMATRIKSTxt(Map, 10, 20, "../map1.txt");
+}
+
+void initPosisi (MATRIKS *MAP, POINT *PPlayer, POINT *POffice, POINT *PAntrian) {
+  *PPlayer = getPlayer(*MAP);
+  *POffice = getOffice(*MAP);
+  *PAntrian = getAntrian(*MAP);
+}
+
+char kata[50];
+void concatNama() {
+  STARTKATA();
+  int j=0; int i;
+  while (!EndKata) {
+    for (i=0; i<CKata.Length; i++) {
+      kata[i+j] = CKata.TabKata[i];
+    }
+    kata[i+j] = ' ';
+    j+=CKata.Length+1;
+    ADVKATA();
+  }
+  printf("%s",kata);
+}
+
+void printMap (MATRIKS M) {
+/* Mencetak Map dan Legenda */
+
+    //KAMUS LOKAL
+    int i,j;
+
+    //ALGORITMA
+    TulisMATRIKS(M);
+    printf("Legend:\n");
+    printf("A = Antrian\n");
+    printf("P = Player\n");
+    printf("W = Wahana\n");
+    printf("O = Office\n");
+}
---
+++ main.h
@@ -0,0 +1,36 @@
+/* File: main.h */
+/* *** Definisi MAIN PROGRAM *** */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "boolean.h"
+#include "mesin/mesinkar.h"
+#include "mesin/mesinkata.h"
+#include "matriks/matriks.h"
+#include "point/point.h"
+
+/******* DEKLARASI FUNGSI-FUNGSI UTAMA *******/
+void initGame();
+/* Menginisiasi mulainya permainan Willy Wangky's World.
+   I.S. Game belum dimulai.
+   F.S. Game dimulai dengan input pilihan di main menu. */
+
+void initMap();
+/* Menginisiasi map awal permainan Willy Wangky's World.
+   I.S. Game belum dimulai.
+   F.S. Game dimulai dengan map awal yang terdefinisi di file eksternal. */
+
+void concatNama();
+/* Menerima input dan menyatukan kata-kata yang diakuisisi oleh mesin kata ke dalam satu array of character.
+   I.S. Input sembarang.
+   F.S. Didapat sebuah array of character yang berisi kata-kata terakuisisi dengan dipisahkan spasi. */
+
+/******* FUNGSI-FUNGSI MAP *******/
+void printMap (MATRIKS M) {
+/* Mencetak Map dan Legenda */
+
+#endif
